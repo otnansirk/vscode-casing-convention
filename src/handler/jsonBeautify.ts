5b779de83910d48156e5e30ee819092a7c44a846
k15 <krisnantobiyuh@gmail.com>
2024-12-01 07:35:16 +0700

    fix : refactor json beautify

--- src/handler/jsonBeautify.ts
+++ src/handler/jsonBeautify.ts
@@ -1,8 +1,4 @@
 /**
  * Transfor JSON to beautify.
  */
-export const jsonBeautify = (text: string) => {
-    console.log(JSON.stringify(JSON.parse(text), undefined, 4));
-    
-    return JSON.stringify(JSON.parse(text), undefined, 4)
-};
+export const jsonBeautify = (text: string) => JSON.stringify(JSON.parse(text), undefined, 4);
