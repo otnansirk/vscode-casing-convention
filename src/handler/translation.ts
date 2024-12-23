833703b624d6279fac6c37489a1e46d262df2936
otnansirk <iam.otnansirk@gmail.com>
2024-12-23 20:40:48 +0700

    feat : translation

--- src/handler/translation.ts
+++ src/handler/translation.ts
@@ -1,15 +1,45 @@
-// import { randomUUID } from 'crypto';
-// import * as token from 'google-translate-token';
-// const got = require('got');
-
+import { json2UrlParams } from '../helpers/json2UrlParams';
+import { languages } from '../helpers/languages';
+import * as token from 'google-translate-token';
+import { randomUUID } from 'crypto';
+import * as vscode from 'vscode';
 
 
 export const translation = async (text: string) => {
-
-    // const gtranslateToken = await token.get(randomUUID());
-    // const data = await got.post('https://google.com');
-    
-    // console.log(data.body, randomUUID());
+    try {
       
-      return "asdsads";
+      let targetLanguage = await vscode.window.showQuickPick(languages, {
+        placeHolder: "Select language",
+        canPickMany: false
+      });
+
+      const q = text.split("\n");
+      const gtranslateToken = await token.get(randomUUID());
+      const queryObject = {
+        dt: ['t', 'bd', 'qc', 'rm', 'ex'],
+        client: 'gtx',
+        hl: 'en',
+        sl: 'auto',
+        tl: targetLanguage?.value,
+        dj: '1',
+        mode: '1',
+        sp: 'nmt',
+        tc: '2',
+        tk: gtranslateToken,
+        q
+      };
+
+      const params   = json2UrlParams(queryObject);
+      const response = await fetch(`https://translate.googleapis.com/translate_a/t?${params}`, {method: 'POST'});
+      const data = await response.json();
+      const tx = data.map((items:Array<[string, string]>) => {
+        if (items[0]) {
+          return items[0];
+        }
+      });
+
+      return tx.join("\n");
+    } catch (error) {
+      console.log(error, "KRIS");
+    }    
 };
---
+++ src/helpers/json2UrlParams.ts
@@ -0,0 +1,17 @@
+/**
+ * Convert json to url params
+ * 
+ * @param objData object
+ * @returns 
+ */
+export const json2UrlParams = (objData: object) => {
+    const params = new URLSearchParams();
+    Object.entries(objData).forEach(([key, value]) => {
+    if (Array.isArray(value)) {
+        value.forEach(val => params.append(key, val));
+    } else {
+        params.append(key, value);
+    }
+    });
+    return params.toString();    
+};
