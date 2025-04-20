7bdf44199f0770f00588897c773ac4dd3d98e36a
otnansirk <iam.otnansirk@gmail.com>
2025-04-20 18:12:20 +0700

    fix : lower case first; handle newline and space

--- src/commands/handler/lowerCaseFirst.ts
+++ src/commands/handler/lowerCaseFirst.ts
@@ -2,5 +2,15 @@
  * Lower case the first character of an input string.
  */
 export function lowerCaseFirst(str: string) {
-  return str.toUpperCase().charAt(0).toLowerCase() + str.slice(1);
+  return str.split('\n')
+    .map(line => {
+      const withSpace = line.replace(/([a-z])([A-Z])/g, '$1 $2');
+      const match = withSpace.match(/^(\s*)(\S)(.*)$/);
+
+      if (!match) { return line; }
+
+      const [, spaces, firstChar, rest] = match;
+      return spaces + firstChar.toLowerCase() + rest.toUpperCase();
+    })
+    .join('\n');
 }
