38e4198f3515ce9c61594f224140d95a3082b755
otnansirk <iam.otnansirk@gmail.com>
2025-04-21 21:51:42 +0700

    fix : snake case; handle newline and space

--- src/commands/handler/snakeCase.ts
+++ src/commands/handler/snakeCase.ts
@@ -1,7 +1,19 @@
 /**
  * Transform text to snake_case
  */
-export const snakeCase = (text: string): string => text
-                                                        .replace(/([a-z])([A-Z])/g, '$1_$2')
-                                                        .replace(/[-. \t\\]+/g, "_")
-                                                        .toLowerCase();
+export const snakeCase = (text: string): string => {
+    return text.split('\n')
+        ?.map((str) => {
+            const leadingSpace = str.match(/^(\s*)/)?.[0] ?? "";
+            const result = str
+                .replace(/^(\s+)/, '')
+                .replace(/([a-z])([A-Z])/g, '$1_$2')
+                .replace(/[-. \t\\]+/g, "_")
+                .replace(/[^a-zA-Z0-9_]/g, '')
+                .replace(/[_]+/g, "_")
+                .replace(/[\s_]+$/, '') // Remove space and - in end of character
+                .toLowerCase();
+            return leadingSpace + result;
+        })
+        .join('\n') ?? '';
+}
