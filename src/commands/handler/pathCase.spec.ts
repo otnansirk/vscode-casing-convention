3f6550cc148d0aee9c15c7a2a4ae146196fd9637
otnansirk <iam.otnansirk@gmail.com>
2025-04-22 17:58:04 +0700

    fix : unittest path case

---
+++ src/commands/handler/pathCase.spec.ts
@@ -0,0 +1,49 @@
+import { strictEqual } from "assert";
+import { pathCase } from "./pathCase";
+
+describe('pathCase', function () {
+
+  const TEST_CASES = [
+    {
+      input: "Javascript",
+      expected: "javascript"
+    },
+    {
+      input: "Hi,kris how are you ?",
+      expected: "hi/kris/how/are/you"
+    },
+    {
+      input: "Hi, KRIS HowAreYou",
+      expected: "hi/kris/how/are/you"
+    },
+    {
+      input: "KRIS_HowAreYOU",
+      expected: "kris/how/are/you"
+    },
+    {
+      input: "HOW ARE YOU",
+      expected: "how/are/you"
+    },
+    {
+      input: "How Are YOU",
+      expected: "how/are/you"
+    },
+    {
+      input: `   How Are YOU
+      hOW ARE YOU`,
+      expected: `   how/are/you
+      h/ow/are/you`
+    },
+    {
+      input: " ",
+      expected: " "
+    },
+  ];
+
+  TEST_CASES.forEach(({ input, expected }: any) => {
+    it(`Sould convert ${input} to ${expected}`, function () {
+      let result = pathCase(input);
+      strictEqual(result, expected);
+    });
+  });
+});
