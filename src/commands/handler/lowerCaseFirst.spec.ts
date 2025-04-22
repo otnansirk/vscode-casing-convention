ea0c0b4fc68a8fdb7464329d33e76d03cf79672d
otnansirk <iam.otnansirk@gmail.com>
2025-04-22 07:46:00 +0700

    fix : unittest lower case first

--- src/commands/handler/lowerCaseFirst.spec.ts
+++ src/commands/handler/lowerCaseFirst.spec.ts
@@ -1,24 +1,24 @@
 import { strictEqual } from "assert";
 import { lowerCaseFirst } from "./lowerCaseFirst";
 
-describe('lowerCaseFirst', function() {
+describe('lowerCaseFirst', function () {
 
   const TEST_CASES = [
     {
       input: "Javascript",
-      expected: "javascript"
+      expected: "jAVASCRIPT"
     },
     {
       input: "Hi,kris how are you ?",
-      expected: "hi,kris how are you ?"
+      expected: "hI,KRIS HOW ARE YOU ?"
     },
     {
       input: "Hi, KRIS HowAreYou",
-      expected: "hi, KRIS HowAreYou"
+      expected: "hI, KRIS HOW ARE YOU"
     },
     {
       input: "KRIS_HowAreYOU",
-      expected: "kRIS_HowAreYOU"
+      expected: "kRIS_HOW ARE YOU"
     },
     {
       input: "HOW ARE YOU",
@@ -26,12 +26,18 @@ describe('lowerCaseFirst', function() {
     },
     {
       input: "How Are YOU",
-      expected: "how Are YOU"
+      expected: "hOW ARE YOU"
+    },
+    {
+      input: `   How Are YOU
+      hOW ARE YOU`,
+      expected: `   hOW ARE YOU
+      h OW ARE YOU`
     },
   ];
 
-  TEST_CASES.forEach(({input, expected}: any) => {
-    it(`Sould convert ${input} to ${expected}`, function() {
+  TEST_CASES.forEach(({ input, expected }: any) => {
+    it(`Sould convert ${input} to ${expected}`, function () {
       let result = lowerCaseFirst(input);
       strictEqual(result, expected);
     });
